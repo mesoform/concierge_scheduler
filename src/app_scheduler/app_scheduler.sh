b69ea76ca62d89688214e2d785b0d15eb76804ec
Gareth Brown <gareth@mesoform.com>
2017-03-19 00:25:25 +0000

    certificates had to be stored with the script (or at least not in /root) and modified docker-compose command to pass parameters

--- src/app_scheduler/app_scheduler.sh
+++ src/app_scheduler/app_scheduler.sh
@@ -5,7 +5,7 @@
 # Environment setup
 cd "$(dirname "$0")"
 shopt -s expand_aliases
-source docker_env.sh
+. ./docker_env.sh
 
 # Variable assignment
 action=$1
@@ -14,7 +14,12 @@ current_scale=$3
 increment=$4
 
 scale_service(){
-    docker-compose -f /tmp/docker-compose.yml -p dockerlx scale ${service_name}=$1
+    /usr/bin/docker-compose --tlsverify --tlscert=${DOCKER_CERT_PATH}cert.pem \
+       --tlscacert=${DOCKER_CERT_PATH}ca.pem --tlskey=${DOCKER_CERT_PATH}key.pem \
+       --host tcp://dockerapi-private-lab1.mesoform.com:2376 --file /tmp/docker-compose.yml --project-name dockerlx \
+       scale ${service_name}=$1
+    echo "$(date): Scaled ${service_name} from ${current_scale} to $1" >> /tmp/app_scheduler_output
+    exit 0
 }
 
 scale_up(){
--- src/app_scheduler/docker_env.sh
+++ src/app_scheduler/docker_env.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env bash
-export DOCKER_CERT_PATH=/root/.sdc/docker/gaz
+export DOCKER_CERT_PATH=./
 export DOCKER_HOST=tcp://dockerapi-private-lab1.mesoform.com:2376
 export DOCKER_CLIENT_TIMEOUT=300
 export COMPOSE_HTTP_TIMEOUT=300
-alias docker="docker --tls"
